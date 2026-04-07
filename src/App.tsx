b6bdc9568455de8465f410172dc0245b20cf88e6
EvilDron09 <andrej32118@gmail.com>
2026-04-07 21:44:54 +0300

    hw 1.1 #gi01MZ6v

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-
 import './App.css'
+
+const coursesTitleArray: string[] = [
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'Fullstack',
+  'Frontend'
+];
 
 function App() {
 
-
   return (
     <>
+      <ul>
+        {
+          coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)
+        }
+      </ul>
     </>
   )
 }
