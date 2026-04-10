316d3f24c4e6f71dfbc4b39f714c7974a32128e0
EvilDron09 <andrej32118@gmail.com>
2026-04-10 19:43:21 +0300

    lesson 2.1 useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 
 import './App.css'
+import {useState} from "react";
 
-function App() {
+const App = () => {
 
-
+let [counter, setCounter] = useState<number>(0);
+console.log('mount')
   return (
-    <>
-    </>
+    <div>
+      <h2>{counter}</h2>
+      <button onClick={() =>{
+        setCounter(counter++);
+      }}>increment</button>
+      <button onClick={()=>{
+        setCounter(prevState => {
+          return --prevState;
+        })
+      }}>decrement</button>
+    </div>
   )
 }
 
