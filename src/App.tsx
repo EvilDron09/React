4e05dfbe73998ebc90bffd4cd324e936be500fb2
EvilDron09 <andrej32118@gmail.com>
2026-05-04 21:13:20 +0300

    lesson 7 memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import {UsersComponent} from "./components/users-component/UsersComponent.tsx";
+
 
 function App() {
 
 
   return (
-    <>
-    </>
+    <div>
+      <UsersComponent/>
+    </div>
   )
 }
 
