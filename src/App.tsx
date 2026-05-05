61673329483f059a34152762acf5776940264787
EvilDron09 <andrej32118@gmail.com>
2026-05-05 20:20:17 +0300

    lesson 7.5 context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 
 import './App.css'
+import {LeftBranch} from "./component/LeftBranch.tsx";
+import {RightBranch} from "./component/RightBranch.tsx";
+import {init, MyContext} from "./context/MyContext.tsx";
+import {useState} from "react";
 
 function App() {
 
+    const [counter, setCounter] =useState<number>(init.counterValue)
 
   return (
     <>
+      <MyContext.Provider value={{
+        counterValue:counter  ,
+        increment:(obj)=>{
+          setCounter(++obj)
+        }
+      }}>
+        <LeftBranch/>
+        <RightBranch/>
+      </MyContext.Provider>
+
     </>
   )
 }
