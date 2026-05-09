a9165ee4c7d5cf7b5d73f9e6ec2faaae9646f2bb
EvilDron09 <andrej32118@gmail.com>
2026-05-09 17:36:03 +0300

    lesson 8.2 Redux toolkit . asyncThunk

---
+++ src/redux/userSlice/userSlice.ts
@@ -0,0 +1,41 @@
+import type {IUser} from "../../models/IUser.ts";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+
+type UserSliceType ={
+    users:IUser[]
+}
+
+const initialState:UserSliceType ={users: []};
+
+const loadUsers = createAsyncThunk('userSlice/loadUsers',
+    async (_ ,thunkAPI) => {
+        try {
+            const users = await fetch('https://jsonplaceholder.typicode.com/users')
+                .then(value => value.json())
+            return thunkAPI.fulfillWithValue(users);
+        }catch (e){
+            console.log(e)
+            return thunkAPI.rejectWithValue('some error')
+        }
+    }
+
+)
+
+
+export const userSlice = createSlice({
+    name: "userSlice",
+    initialState: initialState,
+    reducers:{},
+    extraReducers:builder =>
+        builder.addCase(loadUsers.fulfilled,(state, action)=>{
+            state.users = action.payload
+        })
+            .addCase(loadUsers.rejected, (state,action) =>{
+                console.log(state);
+                console.log(action);
+            })
+});
+
+export const userSliceActions ={
+    ...userSlice.actions, loadUsers
+}
